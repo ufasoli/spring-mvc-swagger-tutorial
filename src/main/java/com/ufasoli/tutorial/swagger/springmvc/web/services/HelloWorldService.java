8028d07d1f8471cdcf3a1e93b6a9ef7f699e81e1
Ulises Fasoli <ufasoli@gmail.com>
2013-06-06 15:38:58 +0200

    Added HelloWorldService

---
+++ src/main/java/com/ufasoli/tutorial/swagger/springmvc/web/services/HelloWorldService.java
@@ -0,0 +1,25 @@
+package com.ufasoli.tutorial.swagger.springmvc.web.services;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * User: ufasoli
+ * Date: 06/06/13
+ * Time: 15:19
+ * project : spring-mvc-swagger-tutorial
+ */
+@Controller
+@RequestMapping(value = "/hello", produces = MediaType.TEXT_PLAIN_VALUE)
+public class HelloWorldService {
+
+    @RequestMapping( produces = MediaType.TEXT_PLAIN_VALUE, method = RequestMethod.GET)
+    public @ResponseBody String sayHelloWorld(){
+
+        return "Hello World!";
+    }
+}
