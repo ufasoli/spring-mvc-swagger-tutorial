96ea3e33bab3f68870d6025af38a8d7fb562e2e2
Ulises Fasoli <ufasoli@gmail.com>
2013-06-06 17:15:23 +0200

    deleted some clutter

--- src/main/java/com/ufasoli/tutorial/swagger/springmvc/web/services/HelloWorldService.java
+++ src/main/java/com/ufasoli/tutorial/swagger/springmvc/web/services/HelloWorldService.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * User: ufasoli
